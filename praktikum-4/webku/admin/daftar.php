43418cc725c7cde72a0f9717112c17554ed205ad
rndprtm <148959144+rndprtm@users.noreply.github.com>
2024-03-20 10:58:09 +0700

    Add files via upload

---
+++ praktikum-4/webku/admin/daftar.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+?>
+<div>
+    <div class="container-fluid px-4">
+        <h1>Silahkan Isi Form Pendaftaran</h1>
+</div>
+</div>
+<?phpinclude_once 'bottom.php';
+?>
---
+++ praktikum-4/webku/admin/index.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+?>
+<div>
+    <div class="container-fluid px-4">
+        <h1>Selamat Datang di halaman Admin</h1>
+</div>
+</div>
+<?phpinclude_once 'bottom.php';
+?>
